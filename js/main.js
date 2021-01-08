9d2e2a9f55e9eedc047f154e24eddc154b0d75f2
Whitney Rosenberg <whitney.rosenberg17@gmail.com>
2021-01-07 23:21:50 -0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+	let list = document.getElementById('table_of_contents');
+
+	const tableOfContents = [
+		{
+			label: 'Week 1 Work',
+			url: 'week1/index.html'
+		}
+	];
+
+	for (var i = tableOfContents.length - 1; i >= 0; i--) {
+		let li = document.createElement('li');
+		let a = document.createElement('a');
+		a.textContent = tableOfContents[i].label;
+		a.setAttribute('href', tableOfContents[i].url);
+		li.append(a);
+		list.append(li);
+		
+	}
+}
+
