f96a0ddc20c3b737448ee390f39ce8db8235e167
Whitney Rosenberg <whitney.rosenberg17@gmail.com>
2021-01-16 21:46:41 -0500

    Chapters 3 and 4 notes

--- week2/quiz/main.js
+++ week2/quiz/main.js
@@ -1,7 +1,31 @@
-window.onload = function () {
-	alert('Welcome to Quiz Ninja!');
-
-	const question = "What is Superman's real name?"
-	const answer = prompt(question);
-	alert(`You answered ${answer}`);	
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+function start(quiz){
+    let score = 0;
+    // main game loop
+    for(const [question,answer] of quiz){
+        const response = ask(question);
+        check(response,answer);
+    }
+    // end of main game loop
+    gameOver();
+    // function declarations
+    function ask(question){
+        return prompt(question);
+    }
+    function check(response,answer){
+        if(response.toLowerCase() === answer.toLowerCase()){
+        alert('Correct!');
+        score++;
+        } else {
+        alert(`Wrong! You answered ${response}. The correct answer was ${answer}`);
+        }
+    }
+    function gameOver(){
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+    }
 }
+window.onload = start(quiz);
