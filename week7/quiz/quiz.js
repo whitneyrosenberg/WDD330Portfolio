831a0ab87b76e92cb2076aa92d8958c79d71fdbc
Whitney Rosenberg <whitney.rosenberg17@gmail.com>
2021-02-20 20:53:12 -0500

    chapter 13 reading

--- week7/quiz/quiz.js
+++ week7/quiz/quiz.js
@@ -1,3 +1,11 @@
+const url = 'http://spbooks.github.io/questions.json';
+fetch(url)
+  .then(res => res.json())
+  .then(quiz => {
+      view.start.addEventListener('click', () => game.start(quiz.questions), false);
+      view.response.addEventListener('click', (event) => game.check(event), false);
+  });
+
 const quiz = [
               { name: "Superman",realName: "Clark Kent" },
               { name: "Wonderwoman",realName: "Dianna Prince" },
