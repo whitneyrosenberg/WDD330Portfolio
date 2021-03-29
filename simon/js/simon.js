efbee0e8dd1fdb63eb04c00e9aea75a75cb32d65
Whitney Rosenberg <whitney.rosenberg17@gmail.com>
2021-03-29 13:02:31 -0400

    added high score functionality

--- simon/js/simon.js
+++ simon/js/simon.js
@@ -2,18 +2,29 @@ import SimonGame from './SimonGame.js';
 import {
     qs,
     onTouch
-} from './utilities.js'
+} from './utilities.js';
+import {
+    readFromLS,
+    writeToLS
+} from './ls.js';
 
 const controller = {
     load() {
         this.simonGame = new SimonGame();
         this.currentMove = 0;
         this.currentScore = 0;
+        this.highScore = readFromLS('highScore') ? readFromLS('highScore') : 0;
+        this.highScorer = readFromLS('name') ? readFromLS("name") : ' ';
         let groups = Array.from(qs('g'));
         let that = this;
         window.addEventListener('load', () => {
             const startButton = qs('#start-game')[0];
             const popupClose = qs("#popup-close-button")[0];
+
+            if (this.highScore > 0) {
+                qs('#high-score')[0].innerHTML = `High Score <br>${this.highScorer}: ${this.highScore}`;
+            }
+
             onTouch(startButton, () => {
                 startButton.classList.toggle('hidden', true);
                 that.simonGame.playCurrentGame();
@@ -21,6 +32,12 @@ const controller = {
             onTouch(popupClose, () => {
                 qs('#hover_bkgr_fricc')[0].classList.toggle('hidden', true);
                 startButton.classList.toggle('hidden', false);
+            });
+            onTouch(qs('button')[0], (event) => {
+                event.preventDefault();
+                let name = qs('#high-scorer')[0].value;
+                writeToLS('name', name);
+                location.reload();
             });
         });
         groups.forEach((group) => {
@@ -55,6 +72,9 @@ function handleUserMove(userMove) {
 function gameOver() {
     controller.currentMove = 0;
     controller.simonGame.resetCurrentGame();
+    if(controller.highScore < controller.currentScore) {
+        writeToLS('highScore', controller.currentScore);
+    }
     qs('#hover_bkgr_fricc')[0].classList.toggle('hidden', false);
     qs("#final-score")[0].innerHTML = `Final Score: ${controller.currentScore}`;
     controller.currentScore = 0;
